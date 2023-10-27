1b2a00c7ac2375ec4876024bf1f00fe933cfa8cd
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-10-27 00:59:57 -0600

    Use container for deployment 🐳

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import preact from "@astrojs/preact";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://joaquinbadillo.github.io',
-  base: '/TC2002S',
   integrations: [preact()],
   output: "server",
   adapter: node({
