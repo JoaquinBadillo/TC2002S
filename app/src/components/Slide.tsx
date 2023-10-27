6e3f94c490046d0704ec960f0f0684f3556a8917
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-10-26 20:38:21 -0600

    Add export to pdf option

--- app/src/components/Slide.tsx
+++ app/src/components/Slide.tsx
@@ -1,20 +1,49 @@
+import { jsPDF } from "jspdf";
+import { useRef } from "preact/hooks";
+
 export default function Slide({title, summary, img}: {title: string, summary: string, img: any}) {
     if (!title || !summary || !img)
         return null;
+
+    const slideRef = useRef<any>(null);
+
+    const SlideToPDF = () => {
+        if (!slideRef.current)
+            return;
+
+        const doc = new jsPDF({
+          orientation: "landscape",
+          format: [1248, 702],
+          unit: "px"
+        });
+
+        doc.html(slideRef.current, {
+            async callback(doc) {
+                if (doc.internal.pages.length > 1)
+                    doc.deletePage(2);
+                
+                doc.save("Slide");
+            }
+
+        });
+    };
     
     return (
         <>
         <hr/>
-        <section className="slide-content">
-            <div className="summary">
-                <h2 className="title">{title}</h2>
-                <p>{summary}</p>
-            </div>
-            <img 
-              src={`data:image/jpeg;base64,${img}`} 
-              alt="Generated Image" 
-            />
-        </section>
+        <div className="content-wrapper">
+            <section className="slide-content" ref={slideRef}>
+                <div className="summary">
+                    <h2 className="title">{title}</h2>
+                    <p>{summary}</p>
+                </div>
+                <img 
+                src={`data:image/jpeg;base64,${img}`} 
+                alt="Generated Image" 
+                />
+            </section>
+            <button onClick={SlideToPDF} >Export</button>
+        </div>
         </>
     );
 }
